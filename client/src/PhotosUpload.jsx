b599d9159ecc8a267bb95bf482ae80216fad2fa8
JKukielski <jankukielski1@gmail.com>
2023-05-28 13:19:39 +0200

    additional image functionality (delete)

--- client/src/PhotosUpload.jsx
+++ client/src/PhotosUpload.jsx
@@ -1,27 +1,28 @@
 /* eslint-disable react/prop-types */
-import { CloudArrowUpIcon } from '@heroicons/react/24/outline';
+import { CloudArrowUpIcon, TrashIcon } from '@heroicons/react/24/outline';
 import axios from 'axios';
 import { useState } from 'react';
 
 export default function PhotosUpload({ addedPhotos, setAddedPhotos }) {
   const [link, setLink] = useState('');
 
-  const addPhotoByLink = async (e) => {
-    e.preventDefault();
-    const { data: fileName } = await axios.post('/upload-by-link', {
+  async function addPhotoByLink(ev) {
+    ev.preventDefault();
+    const { data: filename } = await axios.post('/upload-by-link', {
       link: link,
     });
-    setAddedPhotos((prev) => [...prev, fileName]);
+    setAddedPhotos((prev) => {
+      return [...prev, filename];
+    });
     setLink('');
-  };
-
-  const uploadPhoto = async (e) => {
-    const files = e.target.files;
+  }
+  function uploadPhoto(ev) {
+    const files = ev.target.files;
     const data = new FormData();
     for (let i = 0; i < files.length; i++) {
-      data.append('photos', files[i]);
+      data.append('photos[]', files[i]);
     }
-    await axios
+    axios
       .post('/upload', data, {
         headers: { 'Content-Type': 'multipart/form-data' },
       })
@@ -31,7 +32,12 @@ export default function PhotosUpload({ addedPhotos, setAddedPhotos }) {
           return [...prev, ...filenames];
         });
       });
+  }
+
+  const removePhoto = (filename) => {
+    setAddedPhotos([...addedPhotos.filter((photo) => photo !== filename)]);
   };
+
   return (
     <>
       <h2 className="text-2xl mt-4">Photos</h2>
@@ -52,11 +58,23 @@ export default function PhotosUpload({ addedPhotos, setAddedPhotos }) {
       <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
         {addedPhotos?.length > 0 &&
           addedPhotos.map((link) => (
-            <div key={link} className="h-32 flex">
+            <div key={link} className="h-32 flex relative">
               <img
                 className="rounded-2xl w-full object-cover"
                 src={`http://localhost:4000/uploads/${link}`}
               />
+              <button
+                onClick={() => removePhoto(link)}
+                className="cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-50 py-2 px-3 rounded-xl"
+              >
+                <TrashIcon className="w-6 h-6" />
+              </button>
+              <button
+                onClick={() => selectAsMain(link)}
+                className="cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-50 py-2 px-3 rounded-xl"
+              >
+                <TrashIcon className="w-6 h-6" />
+              </button>
             </div>
           ))}
         <label className="h-32 cursor-pointer flex justify-center gap-2 border bg-transparent rounded-2xl p-2 items-center text-2xl text-gray-600">
